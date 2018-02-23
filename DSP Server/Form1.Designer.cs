eaa926115e74d5dc308e9229e702d4fdf04c4790
AhmadAlHabbash <ahmado-1994@hotmail.com>
2018-02-23 16:21:30 +0200

    Add project files.

---
+++ DSP Server/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace DSP_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.StopServer = new System.Windows.Forms.Button();
+            textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startServer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StopServer
+            // 
+            this.StopServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StopServer.Location = new System.Drawing.Point(164, 29);
+            this.StopServer.Name = "StopServer";
+            this.StopServer.Size = new System.Drawing.Size(129, 51);
+            this.StopServer.TabIndex = 10;
+            this.StopServer.Text = "Stop Server";
+            this.StopServer.UseVisualStyleBackColor = true;
+            this.StopServer.Click += new System.EventHandler(this.StopServer_Click);
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            textBox2.Location = new System.Drawing.Point(12, 137);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new System.Drawing.Size(281, 308);
+            textBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(90, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Clients State";
+            // 
+            // startServer
+            // 
+            this.startServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startServer.Location = new System.Drawing.Point(12, 29);
+            this.startServer.Name = "startServer";
+            this.startServer.Size = new System.Drawing.Size(129, 51);
+            this.startServer.TabIndex = 7;
+            this.startServer.Text = "Start Server";
+            this.startServer.UseVisualStyleBackColor = true;
+            this.startServer.Click += new System.EventHandler(this.startServer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 457);
+            this.Controls.Add(this.StopServer);
+            this.Controls.Add(textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.startServer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DSP Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StopServer;
+        public static System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startServer;
+    }
+}
+
---
+++ DSP Server/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+
+namespace DSP_Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public TcpListener server;
+        public bool work;
+        public int port;
+        public Thread listenerThread;
+        public static int clientNum;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            work = true;
+            port = 1025;
+        }
+
+        private void startServer_Click(object sender, EventArgs e)
+        {
+            // Initialize Server
+            server = new TcpListener(new IPEndPoint(IPAddress.Any, port));
+            server.Start();
+            textBox2.Text = textBox2.Text + "Server Start !!" + Environment.NewLine;
+            // Initialize Listener
+            listenerThread = new Thread(new ThreadStart(Listening));
+            listenerThread.IsBackground = true;
+            listenerThread.Start();
+        }
+
+        private void StopServer_Click(object sender, EventArgs e)
+        {
+            // Stop Server
+            work = false;
+            server.Stop();
+            server = null;
+            textBox2.Text = textBox2.Text + "Server Stop !!" + Environment.NewLine;
+            // Listener is OFF
+            listenerThread.Abort();
+            listenerThread = null;
+        }
+
+        private void Listening()
+        {
+            while (work)
+            {
+                try
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    new Thread(new ThreadStart(() => HandleClient(client))).Start();
+                    clientNum++;
+                }
+                catch (Exception ex)
+                {
+                    Console.Write("No Client Connection !!");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public static void HandleClient(TcpClient client)
+        {
+            textBox2.Invoke(new Action(() => textBox2.Text = textBox2.Text + "new Client" + Environment.NewLine));
+            StreamReader sr = new StreamReader(client.GetStream());
+            char[] data = new char[20];
+
+            JObject message;
+            string createText;
+
+            while (true)
+            {
+                if ((sr.Read(data, 0, data.Length)) > 0)
+                {
+                    message = JObject.Parse(new string(data));
+                    createText = message["lux"].ToString() + Environment.NewLine;
+                    File.AppendAllText("client_" + clientNum + ".xls", createText);
+                }
+                else
+                {
+                    textBox2.Invoke(new Action(() => textBox2.Text = textBox2.Text + "Client Disconnected !!" + Environment.NewLine));
+                    break;
+                }
+                sr.DiscardBufferedData();
+            }
+        }
+    }
+}
